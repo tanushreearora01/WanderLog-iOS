2f2f191bd554ed463007e92824e0e5be9a2c4ea4
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-02-21 14:01:59 -0500

    Initial Commit

---
+++ WanderLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WanderLog
+//
+//  Created by Tarasha Bansal on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WanderLog/WanderLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderLogApp.swift
+//  WanderLog
+//
+//  Created by Tarasha Bansal on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WanderLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
