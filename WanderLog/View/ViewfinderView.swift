aed23b5cfb46e7ff610862885a06d0e8e4d648df
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-03-21 19:36:35 -0400

    Added base code for camera Integration and Splash Screen

---
+++ WanderLog/View/ViewfinderView.swift
@@ -0,0 +1,29 @@
+//
+//  ViewfinderView.swift
+//  WanderLog
+//
+//  Created by Tarasha Bansal on 3/21/24.
+//
+//Got this code from Apple Tutorials from https://developer.apple.com/tutorials/sample-apps/capturingphotos-camerapreview
+import SwiftUI
+
+struct ViewfinderView: View {
+    @Binding var image: Image?
+    
+    var body: some View {
+        GeometryReader { geometry in
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+            }
+        }
+    }
+}
+
+struct ViewfinderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewfinderView(image: .constant(Image(systemName: "pencil")))
+    }
+}
