4d46960abd2658bdfc9cfc074424edcba0b9fa09
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-03-25 19:26:38 -0400

    error fix

--- WanderLog/Social Media/View/Followers.swift
+++ WanderLog/Social Media/View/Followers.swift
@@ -10,14 +10,18 @@ import Firebase
 import FirebaseFirestore
 
 struct SwiftUIView: View {
-    @State private var followingUsernames: [String] = []
+    @State private var followingUsernames = [String]()
     @State private var following = [String]()
-    @State private var usernamesCount: Int = 0
+    @State private var usernamesCount = 0
     let db = Firestore.firestore()
 
     var body: some View {
         
-        NavigationView {
+        VStack{
+            Text("Following (\(usernamesCount))")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+
             List(followingUsernames, id: \.self) { username in
                 HStack {
                     Image(systemName: "person.fill")
@@ -46,60 +50,64 @@ struct SwiftUIView: View {
                     .padding(.bottom, 5)
                 }
             }
-            .navigationTitle("Following (\(usernamesCount))")
-            .onAppear {
-                getFollowing()
-                fetchFollowingUsernames()
-            }
+            .scrollContentBackground(.hidden)
         }
+        .padding()
+        .onAppear {
+            getFollowing()
+        }
+        
     }
     
+    
     func fetchFollowingUsernames() {
-        print(following)
-        db.collection("connections").getDocuments(){(QuerySnapshot, err) in
+        print("following")
+        db.collection("users").getDocuments(){(QuerySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             }else {
                 for document in QuerySnapshot!.documents{
                     if let user = User (id:document.documentID, data: document.data()){
                         for userid in following{
-                            print(userid)
+                            print("Following"+userid)
                             if user.id == userid{
                                 print(user.username)
                                 followingUsernames.append(user.username)
+                                usernamesCount+=1
                             }
                         }
                     }
                 }
+                
             }
         }
     }
     
     func getFollowing(){
-//        if let currentUser = UserManager.shared.currentUser{
-//            print("Logged In")
-//            
+        if let currentUser = UserManager.shared.currentUser{
+            print("Logged In")
+            
             db.collection("connections").getDocuments(){(QuerySnapshot, err) in
                 if let err = err {
                     print("Error getting documents: \(err)")
                 }
                 else {
                     for document in QuerySnapshot!.documents{
-//                        print("\(document.documentID)")
                         if let connection = Connections (id:document.documentID, data: document.data()){
-//                            print("\(connection)")
-                            if(connection.userID1 == "aPMlTRvzUVv1TNCI30cL"){
-                                print(connection.userID1)
+                            if(connection.userID1 == currentUser.id){
+                                print("Step1 :",connection.userID1)
                                 following.append(connection.userID2)
                             }
                         }
                     }
+                    fetchFollowingUsernames()
+                    
                 }
             }
-//        }
-//            else{
-//                print("Not logged in")
-//            }
+        }
+        else{
+            print("Not logged in")
+        }
         
     }
     
