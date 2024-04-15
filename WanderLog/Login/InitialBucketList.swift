858a8783542a4481799ca00f36737b268ef0cedd
Tanushree Arora <aroratanushree.28@gmail.com>
2024-04-14 23:34:28 -0400

    Created Initial Bucket List

---
+++ WanderLog/Login/InitialBucketList.swift
@@ -0,0 +1,61 @@
+//
+//  InitialBucketList.swift
+//  WanderLog
+//
+//  Created by Tanushree Arora on 4/14/24.
+//
+
+import SwiftUI
+
+struct BucketListView: View {
+    @State var city = ""
+    @State var country = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                LogoView()
+                Image(systemName: "globe")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                    .padding()
+                Text("Wanderlog")
+                    .font(.largeTitle)
+                Spacer()
+                Text("Create a Bucket List to begin")
+                    .font(.headline)
+                    
+                TextField("City", text: $city)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+                    TextField("Country", text: $country)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                Button() {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("Add another Item")
+                    }
+                }
+                .padding()
+                Button("Sign Up") {
+                    // Handle sign up action
+                }
+//                .buttonStyle(.borderedProminent)
+                .padding()
+                Spacer()
+            }
+        }
+    }
+    
+    
+}
+
+struct BucketListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BucketListView()
+    }
+}
+
